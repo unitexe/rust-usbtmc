4fc08cad6fd7a02773c633fab423dfe00517aa67
unitexe <unitexe70@gmail.com>
2025-01-25 12:37:35 -0600

    Send IDN and SYSTEM_STATUS commands to Siglent SPD 3303C

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,31 @@
 use rust_usbtmc::instrument::Instrument;
-use std::time::Instant;
 
-const VID: u16 = 0x0699;
-const PID: u16 = 0x0368;
+// Identifiers for SIGLENT SPD 3303C
+const VID: u16 = 0xf4ec;
+const PID: u16 = 0x1430;
 
-const BUS: u8 = 0x01;
-const ADDRESS: u8 = 0x01;
+// Commands for SIGLENT SPD 3303C
+const IDN: &str = "*IDN?";
+const SYSTEM_STATUS: &str = "SYSTem:STATus?";
 
 fn main() {
-    // Single device
     let mut instr_single = Instrument::new(VID, PID);
-    let start = Instant::now();
-    instr_single.write("SELECT:CH1 1").unwrap();
-    println!("Ask: {}", instr_single.ask("*IDN?").unwrap());
 
-    let duration = start.elapsed();
-    println!("Time elapsed is: {:?}", duration);
+    // Query the manufacturer, product model, series NO. and software version.
+    // Example output: Siglent Technologies,SPD3303C,SPD3EGGD7R2531,1.02.01.02.02R3,V2.0
+    println!("Ask: {}", instr_single.ask(IDN).unwrap());
 
-    // Multiple devices
-    let mut instr_multiple = Instrument::new_filtered(VID, PID, BUS, ADDRESS);
-
-    let start = Instant::now();
-    instr_multiple.write("SELECT:CH1 1").unwrap();
-    println!("Ask: {}", instr_multiple.ask("*IDN?").unwrap());
-
-    let duration = start.elapsed();
-    println!("Time elapsed is: {:?}", duration);
+    // Query the current working state.
+    // Return info is in hexidecimal format. Binary correspondence relationship:
+    //
+    // Bit      Corresponding State
+    // ----     --------------------
+    // 0        0: CH1 CV mode; 1: CH1 CC mode
+    // 1        0: CH2 CV mode; 1: CH2 CC mode
+    // 2,3      01: Independent mode; 10: Parallel mode; 11: Series mode
+    // 4        0: CH1 OFF 1: CH1 ON
+    // 5        0: CH2 OFF 1: CH2 ON
+    // 
+    // Example output: 0x000C
+    println!("Ask: {}", instr_single.ask(SYSTEM_STATUS).unwrap());
 }
