5086818586ca3733abd1496b25c8fd7d812a540e
unitexe <unitexe70@gmail.com>
2025-02-04 21:58:09 -0600

    Allow user to send status & channel enable/disable commands

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use rust_usbtmc::instrument::Instrument;
+use clap::Parser;
 
 // Identifiers for SIGLENT SPD 3303C
 const VID: u16 = 0xf4ec;
@@ -7,25 +8,30 @@ const PID: u16 = 0x1430;
 // Commands for SIGLENT SPD 3303C
 const IDN: &str = "*IDN?";
 const SYSTEM_STATUS: &str = "SYSTem:STATus?";
+const CH1_ENABLE: &str = "OUTPut CH1,ON";
+const CH1_DISABLE: &str = "OUTPut CH1,OFF";
+const CH2_ENABLE: &str = "OUTPut CH2,ON";
+const CH2_DISABLE: &str = "OUTPut CH2,OFF";
+const CH3_ENABLE: &str = "OUTPut CH3,ON";
+const CH3_DISABLE: &str = "OUTPut CH3,OFF";
+
+#[derive(Parser)]
+#[command(name = "psctrl")]
+#[command(version = "0.1")]
+#[command(about = "Controls a Siglent SPD 3303C")]
+struct Cli {
+    #[arg(long)]
+    cmd: String,
+}
 
 fn main() {
+    let cli = Cli::parse();
     let mut instr_single = Instrument::new(VID, PID);
-
-    // Query the manufacturer, product model, series NO. and software version.
-    // Example output: Siglent Technologies,SPD3303C,SPD3EGGD7R2531,1.02.01.02.02R3,V2.0
-    println!("Ask: {}", instr_single.ask(IDN).unwrap());
-
-    // Query the current working state.
-    // Return info is in hexidecimal format. Binary correspondence relationship:
-    //
-    // Bit      Corresponding State
-    // ----     --------------------
-    // 0        0: CH1 CV mode; 1: CH1 CC mode
-    // 1        0: CH2 CV mode; 1: CH2 CC mode
-    // 2,3      01: Independent mode; 10: Parallel mode; 11: Series mode
-    // 4        0: CH1 OFF 1: CH1 ON
-    // 5        0: CH2 OFF 1: CH2 ON
-    // 
-    // Example output: 0x000C
-    println!("Ask: {}", instr_single.ask(SYSTEM_STATUS).unwrap());
+    let cli_cmd_str = cli.cmd.as_str();
+    
+    match cli_cmd_str {
+        IDN | SYSTEM_STATUS => println!("Ask: {}", instr_single.ask(cli_cmd_str).unwrap()),
+        CH1_ENABLE | CH1_DISABLE | CH2_ENABLE | CH2_DISABLE | CH3_ENABLE | CH3_DISABLE => instr_single.write(cli_cmd_str).unwrap(),
+        _ => println!("Command not supported"),
+    }
 }
